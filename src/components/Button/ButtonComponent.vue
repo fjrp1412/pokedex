8bdc68cfea75678486fd4f0b6d3249e69a710e6e
Francisco Ruiz <fruz.rc@gmail.com>
2022-09-21 16:20:13 -0400

    add button structure and styles. Have to implement logic and testing for the component

---
+++ src/components/Button/ButtonComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="button">
+    <span>{{ text }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ButtonComponent",
+};
+</script>
+
+<script setup>
+import { toRefs } from "vue";
+const props = defineProps(["text"]);
+
+const { text } = toRefs(props);
+</script>
+
+<style scoped>
+.button {
+  width: 300px;
+  height: 50px;
+  background-color: #73d677;
+  color: var(--dark);
+  border-radius: 15px;
+  box-shadow: -1px -54px 22px -7px rgba(0, 0, 0, 0.19) inset;
+  -webkit-box-shadow: -1px -54px 22px -7px rgba(0, 0, 0, 0.19) inset;
+  -moz-box-shadow: -1px -54px 22px -7px rgba(0, 0, 0, 0.19) inset;
+  font-size: 2.8rem;
+  font-weight: bolder;
+}
+</style>
