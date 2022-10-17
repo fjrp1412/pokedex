d45698e8022841675e4bae2537304ad5cf711b51
Francisco Ruiz <fruz.rc@gmail.com>
2022-10-17 14:45:27 -0400

    add Chip component for the PokeCard

---
+++ src/components/Chip/ChipComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :style="cssProps" class="chip">
+    <span class="chip__text"> <slot></slot> </span>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+const props = defineProps({
+  sx: {
+    type: Object,
+  },
+});
+
+const cssProps = computed(() => {
+  return {
+    width: "60px",
+    height: "18px",
+    fontSize: "14px",
+    backgroundColor: "var(--primary)",
+    color: "black",
+    borderRadius: "25px",
+    fontWeight: "bolder",
+    ...props.sx,
+  };
+});
+</script>
+
+<script>
+export default {
+  name: "ChipComponent",
+};
+</script>
+
+<style scoped>
+.chip {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
